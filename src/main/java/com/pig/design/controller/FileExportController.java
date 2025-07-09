b202c27e12339d71dc7012ce709df4ed67548e42
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-07-10 00:31:13 +0800

    控制类

--- src/main/java/com/pig/design/controller/FileExportController.java
+++ src/main/java/com/pig/design/controller/FileExportController.java
@@ -1,4 +1,49 @@
 package com.pig.design.controller;
 
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.StrUtil;
+import lombok.SneakyThrows;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/file")
 public class FileExportController {
+
+    private static final String DIR = "/Users/guochaojun/Desktop/实习文件/设计模式六合一/boot-design/data";
+
+    @SneakyThrows
+    @PostMapping("/upload")
+    public String uploadAndExport(MultipartFile file) {
+        //save file
+        if(Objects.isNull(file)||file.isEmpty()){
+            throw new IllegalArgumentException("文件为空");
+        }
+
+        File saveFile = toSavePath(file.getOriginalFilename()).toFile();
+
+        file.transferTo(saveFile);
+
+        return saveFile.getAbsolutePath();
+    }
+
+    private Path toSavePath(String originalFilename){
+        String filename = IdUtil.fastSimpleUUID();
+
+        String extName = FileUtil.extName(originalFilename);
+        String saveFilename = filename = StrUtil.DOT + extName;
+
+        return Paths.get(DIR, saveFilename);
+    }
 }
